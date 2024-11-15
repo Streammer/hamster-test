220c77ffcbb98f41bb1d606f58ace3f90e83615a
Dmitrij <lukdmitrij@yandex.ru>
2024-11-15 15:25:19 +0300

    create layout of released list

--- src/components/ReleasedList.vue
+++ src/components/ReleasedList.vue
@@ -5,22 +5,19 @@
                 <div v-for="item in data" :key="item.id" class="released-list__item">
                     <div class="released-list__name">{{ item.name }}</div>
                     <div class="released-list__awards">
-                        <div class="released-list__reward">
-                            <span>REWARD</span>
-                            <div v-if="item.reward.medals" class="released-list__points-values">
-                                <img src="@/assets/medal.svg" alt="medal icon">
-                                <span>{{ item.reward.medals }}</span>
-                            </div>
-                            <span>{{ formatNumber(item.reward.points)}}</span>
+                        <span class="released-list__awards-title">REWARD</span>
+                        <div v-if="item.reward.medals" class="released-list__points-values">
+                            <img src="@/assets/medal.svg" alt="medal icon">
+                            <span class="released-list__medals">{{ item.reward.medals }}</span>
                         </div>
-                        <div class="released-list__fun-bonus">
-                            <span>FUN BONUS</span>
-                            <div v-if="item.funBonus.medals" class="released-list__points-values">
-                                <img src="@/assets/medal.svg" alt="medal icon">
-                                <span>{{ item.funBonus.medals }}</span>
-                            </div>
-                            <span>{{ formatNumber(item.funBonus.points)}}</span>
+                        <span class="released-list__points">{{ formatNumber(item.reward.points) }}</span>
+                        <span class="released-list__awards-title" :class="item.funBonus.medals ? 'orange' : 'red'">FUN
+                            BONUS</span>
+                        <div v-if="item.funBonus.medals" class="released-list__points-values">
+                            <img src="@/assets/medal.svg" alt="medal icon">
+                            <span class="released-list__medals">{{ item.funBonus.medals }}</span>
                         </div>
+                        <span class="released-list__points">{{ formatNumber(item.funBonus.points) }}</span>
                     </div>
                 </div>
             </div>
@@ -44,14 +41,14 @@ const data = ref([
     { id: 12, name: 'puzzle', reward: { medals: 200, points: 2500 }, funBonus: { medals: null, points: null } },
     { id: 13, name: 'puzzle', reward: { medals: 200, points: 2500 }, funBonus: { medals: null, points: null } },
 ])
-const  formatNumber = (value)=> { 
-    if (value < 1000) { 
-        return value; 
-    } 
-    else { 
-        const result = value / 1000; 
-        return Number.isInteger(result) ? result + 'k' : result.toFixed(1) + 'k'; 
-    } 
+const formatNumber = (value) => {
+    if (value < 1000) {
+        return value;
+    }
+    else {
+        const result = value / 1000;
+        return Number.isInteger(result) ? result + 'k' : result.toFixed(1) + 'k';
+    }
 }
 </script>
 <style scoped>
@@ -103,7 +100,8 @@ const  formatNumber = (value)=> {
 }
 
 .released-list__name {
-    width: max-content;
+    min-width: max-content;
+    width: 20%;
     color: #FFF;
     text-shadow: 0px 0.579px 0px #000;
     -webkit-text-stroke-width: 0.28973597288131714;
@@ -146,10 +144,54 @@ const  formatNumber = (value)=> {
 .released-list__awards {
     width: 80%;
     margin-left: auto;
+    display: grid;
+    grid-template-columns: minmax(max-content, 1fr) minmax(0, 1fr) minmax(0, 1fr);
 }
+
 .released-list__points-values {
     display: flex;
     align-items: center;
     gap: 4px;
+    justify-self: flex-start;
+    padding-left: 20px;
+}
+
+.released-list__awards-title {
+    color: #FFF;
+    text-shadow: 0px 0.579px 0px #000;
+    -webkit-text-stroke-width: 0.28973597288131714;
+    -webkit-text-stroke-color: var(--stroke-counter, #000);
+    font-family: Lato;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+    text-transform: uppercase;
+    white-space: nowrap;
+}
+
+.released-list__awards-title.orange {
+    color: #F3C209;
+}
+
+.released-list__awards-title.red {
+    color: #FF7957;
+}
+
+.released-list__medals,
+.released-list__points {
+    color: #FFF;
+    text-shadow: 0px 0.5px 0px rgba(0, 0, 0, 0.50);
+    -webkit-text-stroke-width: 0.5;
+    -webkit-text-stroke-color: var(--stroke-counter, #000);
+    font-family: "Movavi Grotesque";
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+}
+.released-list__points {
+    justify-self: flex-start;
+    margin-left: auto;
 }
 </style>
